1adcebef4615be582ade651d45783138c97379e6
llmsilva <86919260+llmsilva@users.noreply.github.com>
2024-10-14 06:52:58 -0300

    Adicionando os arquivos do projeto

---
+++ script.js
@@ -0,0 +1,18 @@
+const nome = "spiderMan";
+let xp = 10002;
+
+if (xp == 1000) {
+  console.log(`O herói ${nome} é nivel Ferro.`);
+} else if (xp > 1001 && xp <= 2000) {
+  console.log(`O herói ${nome} é nível Bronze.`);
+} else if (xp > 2001 && xp <= 5000) {
+  console.log(`O herói ${nome} é nível Prata.`);
+} else if (xp > 5001 && xp <= 7000) {
+  console.log(`O herói ${nome} é nível Ouro.`);
+} else if (xp > 7001 && xp <= 8000) {
+  console.log(`O herói ${nome} é nível Platina.`);
+} else if (xp > 9001 && xp <= 10000) {
+  console.log(`O herói ${nome} é nível Imortal.`);
+} else {
+  console.log(`O herói ${nome} é nível Radiante.`);
+}
